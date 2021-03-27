2cf5d3e79979c996513b7d8cbcf22bb29c85f5ba
Cutler <cutlerthayer@gmail.com>
2021-03-27 16:21:54 -0500

    Took away all unnecessary code

--- movie-scheduler/src/App.js
+++ movie-scheduler/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = 'Cutler';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is the start of my code!!!!!
-        </a>
-      </header>
+    <div className="Container">
+      <h1>Movie Schedule by {name}!</h1>
     </div>
   );
 }
