ebcbbc71333aed1606f8e54845f74e7e16673641
Cutler <cutlerthayer@gmail.com>
2021-03-27 20:19:29 -0500

    Add types to header

--- movie-scheduler/src/Components/Header.js
+++ movie-scheduler/src/Components/Header.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Header = (props) => {
     return (
         <header>
@@ -8,6 +10,10 @@ const Header = (props) => {
 
 Header.defaultProps = {
     title: 'Movie Scheduler'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
 }
 
 export default Header
