2809a0a7505198c869dbb3d5f6d7ee6ae1c8a593
Cutler Thayer <cutlerthayer@gmail.com>
2021-04-12 21:34:47 -0500

    Second part of form added (Fix: putting everything into a nice format)

--- movie-scheduler/src/Components/OpenMovieForm.js
+++ movie-scheduler/src/Components/OpenMovieForm.js
@@ -5,11 +5,21 @@ class OpenMovieForm extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            titleVal: 'Enter movie title'
+            titleVal: 'Enter movie title',
+            notesVal: 'Enter notes about movie'
         };
 
-        // this.handleChange = this.handleChange.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleNotesChange = this.handleNotesChange.bind(this);
         // this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleTitleChange(event){
+        this.setState({titleVal: event.target.value});
+    }
+
+    handleNotesChange(event){
+        this.setState({notesVal: event.target.value});
     }
 
     render(){
@@ -17,8 +27,13 @@ class OpenMovieForm extends React.Component{
             <form className='openMovieForm' onSubmit={this.handleSubmit}>
                 <label>
                     Title:  
-                    <input value={this.state.titleVal} />
+                    <input value={this.state.titleVal} onChange={this.handleTitleChange} />
                 </label>
+                <label>
+                    Notes:  
+                    <input value={this.state.notesVal} onChange={this.handleNotesChange} />
+                </label>
+                <button className='listButton'>Add to list</button>
             </form>
         );
     }
