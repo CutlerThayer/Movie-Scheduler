95d52169c313444cdd25c5c385dcab00d6d4afe0
Cutler Thayer <cutlerthayer@gmail.com>
2021-04-12 20:43:24 -0500

    Refactored all my components to be jsx files instead of js

---
+++ movie-scheduler/src/Components/OpenMovieForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class OpenMovieForm extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+        };
+
+    }
+
+    render(){
+        return{
+            <form>
+            </form>
+        };
+    }
+}
