5cd27a42235d06d9fe3b4efe23336bcaca250181
Cutler Thayer <cutlerthayer@gmail.com>
2021-05-07 20:19:41 -0500

    Finish remove form look

--- movie-scheduler/src/Components/RemoveMovieForm.js
+++ movie-scheduler/src/Components/RemoveMovieForm.js
@@ -1,10 +1,33 @@
-const RemoveMovieForm = () => {
- 
-    return (
-        <div className='removeMovieForm'>
-            <p>Remove movie</p>
-        </div>
-    )
+import React from 'react';
+
+class RemoveMovieForm extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            titleVal: 'Enter movie title',
+            notesVal: 'Enter notes about movie',
+            theaterNum: '2'
+        };
+
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+    }
+
+    handleTitleChange(event){
+        this.setState({titleVal: event.target.value});
+    }
+
+    render(){
+        return(
+            <form className='removeMovieForm' onSubmit={this.handleSubmit}>
+                <label>
+                    Title:  
+                    <input value={this.state.titleVal} onChange={this.handleTitleChange} />
+                </label>
+                <button className='listButton'>Remove from list</button>
+            </form>
+        );
+    }
 }
 
-export default RemoveMovieForm
+export default RemoveMovieForm;
